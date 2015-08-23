fe549d3cb8073dffdcfeb048d2525bae461243db
seboccis <seboccis@orange.fr>
2015-08-23 15:50:37 +0200

    edit layout

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -3,14 +3,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
-
+	
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
 </head>
 <body>
 	<div class="container">
 		<header>
-			<h1>W :: <?= $this->e($title) ?></h1>
+			<h1>BDLoc :: <?= $this->e($title) ?></h1>
 		</header>
+
+		<nav>
+			<a href="<?php echo $this->url('home');?>">Accueil</a>
+			<a href="<?php echo $this->url('catalog');?>">Catalogue</a>
+		</nav>
 
 		<section>
 			<?= $this->section('main_content') ?>
