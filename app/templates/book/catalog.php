f43d198f03f4ce95e1ad0007aed0979d662715fe
seboccis <seboccis@orange.fr>
2015-08-25 09:38:33 +0200

    develop side-bar in catalog.php

--- app/templates/book/catalog.php
+++ app/templates/book/catalog.php
@@ -2,12 +2,24 @@
 
 <?php $this->start('main_content') ?>
 	<div class="sideBar" value="<?= $this->url('ajax_catalog_getBooks');?>">
+		<h2>Filtres</h2>
 
 		<form action="" method='POST' id="formChooseGenres">
 
-		<?php foreach($genres as $genre){	?>
-			<label for="checkbox<?= $genre['id']?>"><input type="checkbox" name="genres[]" id="checkbox<?= $genre['id']?>" value="<?= $genre['id']; ?>"><?= $genre['genre'];?></label>
-		<?php }								?>
+			<div>
+				<h3>Catégories</h3>
+				<?php foreach($genres as $genre){	?>
+					<label for="checkbox<?= $genre['id']?>"><input type="checkbox" name="genres[]" id="checkbox<?= $genre['id']?>" value="<?= $genre['id']; ?>"><?= $genre['genre'];?></label>
+				<?php }								?>
+			</div>
+
+			<div>
+				<h3>Disponibilité</h3>
+			</div>
+
+			<div>
+				<h3>Recherche</h3>
+			</div>
 
 			<button id="buttonChooseGenres">Valider</button>
 
