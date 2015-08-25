56a01b893a821309b1011bee15f94f98a4bb8ac8
NicolasDelille <nicolas.delille@gmail.com>
2015-08-25 09:35:17 +0200

    Add routes for login and register

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -2,5 +2,8 @@
 
 <?php $this->start('main_content') ?>
 	<h2>Let's code.</h2>
+
+	<a href="<?= $this->url('login')?>">Connectez-vous</a>
+	<a href="<?= $this->url('register')?>">Créer un compte</a>
 	
 <?php $this->stop('main_content') ?>
