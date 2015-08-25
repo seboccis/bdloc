776dd9f1e7739896afb28a1e7a3d659a5fa9de73
seboccis <seboccis@orange.fr>
2015-08-25 19:38:41 +0200

    begin css bordered div with title in catalog

--- app/templates/book/catalog.php
+++ app/templates/book/catalog.php
@@ -1,31 +1,37 @@
 <?php $this->layout('catalog_layout', ['title' => 'Catalogue']) ?>
 
 <?php $this->start('main_content') ?>
+
 	<div id="sideBar" data-ajax-catalog-getBooks-path="<?= $this->url('ajax_catalog_getBooks');?>">
-		<h2>Filtres</h2>
+		
+		<div class="borderedWithTitle">
 
-		<form action="" method='POST' id="formSideBarFilters">
+			<h2>Filtres</h2>
 
-			<div>
-				<h3>Catégories</h3>
-				<?php foreach($genres as $genre){	?>
-					<label for="checkbox<?= $genre['id']?>"><input type="checkbox" name="genres[]" class="checkbox" id="checkbox<?= $genre['id']?>" value="<?= $genre['id']; ?>"><?= $genre['genre'];?></label>
-				<?php }								?>
-			</div>
+			<form action="" method='POST' id="formSideBarFilters">
 
-			<div>
-				<h3>Disponibilité</h3>
-				<label for="checkboxAvailability"><input type="checkbox" name="availability" class="checkbox" id="checkboxAvailability" value="1">Disponibles</label>
-			</div>
+				<div>
+					<h3>Catégories</h3>
+					<?php foreach($genres as $genre){	?>
+						<label for="checkbox<?= $genre['id']?>"><input type="checkbox" name="genres[]" class="checkbox" id="checkbox<?= $genre['id']?>" value="<?= $genre['id']; ?>"><?= $genre['genre'];?></label>
+					<?php }								?>
+				</div>
 
-			<div>
-				<h3>Recherche</h3>
-				<input type="text" id="keyword" data="<?php echo $this->url('ajax_catalog_keyword'); ?>" value="">
-				<button>Rechercher</button>				
-				<div id="result"></div>
-			</div>
+				<div>
+					<h3>Disponibilité</h3>
+					<label for="checkboxAvailability"><input type="checkbox" name="availability" class="checkbox" id="checkboxAvailability" value="1">Disponibles</label>
+				</div>
 
-		</form>
+				<div>
+					<h3>Recherche</h3>
+					<input type="text" id="keyword" data="<?php echo $this->url('ajax_catalog_keyword'); ?>" value="">
+					<button>Rechercher</button>				
+					<div id="result"></div>
+				</div>
+
+			</form>
+
+		</div>
 
 	</div>
 
@@ -33,26 +39,30 @@
 
 		<div id="resultsBar">
 
-			<h2>Résultats</h2>
+			<div class="borderedWithTitle">
 
-			<form action="" method='POST' id="formResultsBarFilters" class="elementOfResultsBar">
+				<h2>Résultats</h2>
 
-				<div>
-					<select name="number">
-					  <option value="10">Afficher 10 résultats</option> 
-					  <option value="20" selected>Afficher 20 résultats</option>
-					  <option value="40">Afficher 40 résultats</option>
-					</select>
+				<form action="" method='POST' id="formResultsBarFilters" class="elementOfResultsBar">
+
+					<div>
+						<select name="number">
+						  <option value="10">Afficher 10 résultats</option> 
+						  <option value="20" selected>Afficher 20 résultats</option>
+						  <option value="40">Afficher 40 résultats</option>
+						</select>
+					</div>
+
+					<button id="btnNumber">Valider</button>
+
+				</form>
+
+				<div id="paginationNav" class="elementOfResultsBar">
+					<a href="" id="prevBooks">&lsaquo; Précédentes</a>
+					<span id="pagination">#<span id="first">1</span>à#<span id="last">20</span>sur<span id="total"></span>BD</span>
+					<a href="" id="nextBooks">Suivantes &rsaquo;</a>
 				</div>
 
-				<button id="btnNumber">Valider</button>
-
-			</form>
-
-			<div id="paginationNav" class="elementOfResultsBar">
-				<a href="" id="prevBooks">&lsaquo; Précédentes</a>
-				<span id="pagination">#<span id="first">1</span>à#<span id="last">20</span>sur<span id="total"></span>BD</span>
-				<a href="" id="nextBooks">Suivantes &rsaquo;</a>
 			</div>
 			
 		</div>
