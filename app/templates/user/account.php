ca13a7e06e1a7b33d80ec89aeaf1d41d908bd734
NicolasDelille <nicolas.delille@gmail.com>
2015-08-26 10:55:32 +0200

    Update style on account section

--- app/templates/user/account.php
+++ app/templates/user/account.php
@@ -4,24 +4,25 @@
 
 	<h2>Ceci est la page du compte</h2>
 
-	<nav>
-		<a href="<?php echo $this->url('account')?>">Mes informations</a>
-		<a href="<?php echo $this->url('edit_profile')?>">Modifier mes informations personnelles</a>
-		<a href="<?php echo $this->url('edit_password')?>">Modifier mon mot de passe</a>
-		<a href="#">Consulter mon historique de locations</a>
-		<a href="#">Payer une amende</a>
-		<a href="#">Me désabonner</a>
+	<div id="sideBar" class="navbarAccount">
+		<nav>
+			<a href="<?php echo $this->url('account')?>">Mes informations</a>
+			<a href="<?php echo $this->url('edit_profile')?>">Modifier mes informations personnelles</a>
+			<a href="<?php echo $this->url('edit_password')?>">Modifier mon mot de passe</a>
+			<a href="#">Consulter mon historique de locations</a>
+			<a href="#">Payer une amende</a>
+			<a href="#">Me désabonner</a>
+		</nav>
+	</div>
 
-	</nav>
-
-	<p>Prénom : <?= $w_user['first_name'] ?></p>
-	<p>Nom : <?= $w_user['last_name'] ?></p>
-	<p>Pseudo : <?= $w_user['username'] ?></p>
-	<p>Email : <?= $w_user['email']?></p>
-	<p>Adresse : <?= $w_user['address'] ?></p>
-	<p>Code postal : <?= $w_user['zip_code'] ?></p>
-	<p>Numéro de téléphone : <?= $w_user['phone_number'] ?></p>
-
-
-	
+	<div id="content">
+		<p>Prénom : <?= $w_user['first_name'] ?></p>
+		<p>Nom : <?= $w_user['last_name'] ?></p>
+		<p>Pseudo : <?= $w_user['username'] ?></p>
+		<p>Email : <?= $w_user['email']?></p>
+		<p>Adresse : <?= $w_user['address'] ?></p>
+		<p>Code postal : <?= $w_user['zip_code'] ?></p>
+		<p>Numéro de téléphone : <?= $w_user['phone_number'] ?></p>
+	</div>
+		
 <?php $this->stop('main_content') ?>
--- app/templates/user/edit_password.php
+++ app/templates/user/edit_password.php
@@ -5,34 +5,38 @@
 
 	<h2>Ceci est la page de modification.</h2>
 
-	<nav>
-		<a href="<?php echo $this->url('account')?>">Mes informations</a>
-		<a href="<?php echo $this->url('edit_profile')?>">Modifier mes informations personnelles</a>
-		<a href="<?php echo $this->url('edit_password')?>">Modifier mon mot de passe</a>
-		<a href="#">Consulter mon historique de locations</a>
-		<a href="#">Payer une amende</a>
-		<a href="#">Me désabonner</a>
+	<div id="sideBar" class="navbarAccount">
+		<nav>
+			<a href="<?php echo $this->url('account')?>">Mes informations</a>
+			<a href="<?php echo $this->url('edit_profile')?>">Modifier mes informations personnelles</a>
+			<a href="<?php echo $this->url('edit_password')?>">Modifier mon mot de passe</a>
+			<a href="#">Consulter mon historique de locations</a>
+			<a href="#">Payer une amende</a>
+			<a href="#">Me désabonner</a>
+		</nav>
+	</div>
 
-	</nav>
-
-	<div class="container">
+	<div id="content">
 		<form action="" method="POST">
-			<div class="form-row">
-				<label for="old_password">Ancien mot de passe</label>
-				<input type="password" name="old_password" id="old_password">
-				<p><?= $old_passwordError ?></p>
-			</div>
-			<div class="form-row">
-				<label for="password">Nouveau mot de passe</label>
-				<input type="password" name="password" id="password">	
-			</div>
+			<fieldset>
+				<legend>Modification du mot de passe</legend>
+				<div class="form-row">
+					<label for="old_password">Ancien mot de passe</label>
+					<input type="password" name="old_password" id="old_password">
+					<p><?= $old_passwordError ?></p>
+				</div>
+				<div class="form-row">
+					<label for="password">Nouveau mot de passe</label>
+					<input type="password" name="password" id="password">	
+				</div>
 
-			<div class="form-row">
-				<label for="confirmPassword">Confirmation du nouveau mot de passe</label>
-				<input type="password" name="confirmPassword" id="confirmPassword">	
-				<p><?= $passwordError ?></p>			
-			</div>
+				<div class="form-row">
+					<label for="confirmPassword">Confirmation du nouveau mot de passe</label>
+					<input type="password" name="confirmPassword" id="confirmPassword">	
+					<p><?= $passwordError ?></p>			
+				</div>
 
+			</fieldset>
 
 			<button>Valider</button>
 
