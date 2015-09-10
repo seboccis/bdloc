05ec33f4c4bd0c55e9e0489dbf9d18d5407d73d4
TonySeperoumal <tony.seperoumal@gmail.com>
2015-09-10 16:37:53 +0200

    add font style

--- app/templates/main_layout.php
+++ app/templates/main_layout.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>" />
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<div class="container">
